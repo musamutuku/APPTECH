8ad7497bec9914c7bcb6b59311cec044bc2c8b6e
musamutuku <musajoseph2017@gmail.com>
2021-08-27 16:58:58 +0300

    edited register, login and css files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask.templating import render_template
