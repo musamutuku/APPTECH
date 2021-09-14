41c53b21f790b375bbe9c8752e6ea769a1d244f3
musamutuku <musajoseph2017@gmail.com>
2021-09-14 19:16:12 +0300

    adjusted account-page

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 from sqlalchemy.orm import backref
+import datetime
 
 
 app = Flask(__name__)
